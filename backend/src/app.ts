97c6647d38dd4a604fdd7ddefa3a3ac6de49f278
thiago telini <thiago.telini@hotmail.com>
2026-06-14 20:37:08 -0300

    fixing cors error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
+
 import booksRoutes from "./4-framework/routes/booksRoutes";
 import customerRoutes from "./4-framework/routes/customersRoutes";
 
 const app = express();
 
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/books", booksRoutes);
